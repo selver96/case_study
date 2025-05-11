9ca12beac699d407211f9798730a2820274a67d4
Selver Said <selver.said01@gmail.com>
2025-05-11 16:37:34 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+Main entrypoint for The Dyrt web scraper case study.
+
+Usage:
+    The scraper can be run directly (`python main.py`) or via Docker Compose (`docker compose up`).
+
+If you have any questions in mind you can connect to me directly via info@smart-maple.com
+"""
+
+
+if __name__ == "__main__":
+    print("Hello Smart Maple!")
